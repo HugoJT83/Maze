d61467257e3fd88cfcc1bf00200d557dc76097d3
Hugo Jariod Tomas <hugojariod77@gmail.com>
2026-05-27 23:45:35 +0200

    correccion ultimisima

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/v1' 
+    baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/v1' 
 })
